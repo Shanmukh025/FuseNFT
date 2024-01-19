4fc0c7819ae209ac2979fb9a9f0fa90ba634ded0
Shanmukh025 <shanmukh2564@gmail.com>
2024-01-19 21:31:49 +0530

    added modules

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+return (
+    <>
+    <footer>
+        Made by Shanmukh.
+    </footer>
+    </>
+)
+}
+
+export default Footer
---
+++ src/components/Wallet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Wallet = () => {
+  return (
+    <>
+        <Nav/>
+        Wallet
+    </>
+  )
+}
+
+export default Wallet
