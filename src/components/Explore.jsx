2bceac4ae8e28500186f8082065ac6a525c39da7
Shanmukh025 <shanmukh2564@gmail.com>
2024-01-22 23:20:54 +0530

    commit

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -66,6 +66,23 @@ const Explore = ({ title }) => {
                 </div>
                 <div className="explorecard">
                     <img
+                        src="https://aphrodite.gmanetwork.com/entertainment/articles/480_360_40__20210430124335.jpg"
+                        alt="all"
+                        className="card__img"
+                        style={{ marginTop: "-25px" }}
+                    />
+                    <span className="card__footer">
+                        <span>MEMES</span>
+                        <span>100 NFT's</span>
+                    </span>
+                    <span className="card__action">
+                        <svg viewBox="0 0 448 512" title="play">
+                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+                        </svg>
+                    </span>
+                </div>
+                <div className="explorecard">
+                    <img
                         src="https://i.seadn.io/gcs/files/c5c3ff4a66c4c34f1c93333476162631.jpg?auto=format&dpr=1&h=500"
                         alt="balloon with an emoji face"
                         className="card__img"
@@ -74,6 +91,22 @@ const Explore = ({ title }) => {
                     <span className="card__footer">
                         <span>MUSIC</span>
                         <span>25 NFT's</span>
+                    </span>
+                    <span className="card__action">
+                        <svg viewBox="0 0 448 512" title="play">
+                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+                        </svg>
+                    </span>
+                </div>
+                <div className="explorecard">
+                    <img
+                        src="https://i.seadn.io/s/raw/files/c07c8176faecfe9175f5565f94f83ed6.png?auto=format&dpr=1&w=384"
+                        alt="art"
+                        className="card__img"
+                    />
+                    <span className="card__footer">
+                        <span>PFP</span>
+                        <span>15 NFT's</span>
                     </span>
                     <span className="card__action">
                         <svg viewBox="0 0 448 512" title="play">
@@ -108,39 +141,6 @@ const Explore = ({ title }) => {
                     <span className="card__footer">
                         <span>SPORTS</span>
                         <span>100 NFT's</span>
-                    </span>
-                    <span className="card__action">
-                        <svg viewBox="0 0 448 512" title="play">
-                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                        </svg>
-                    </span>
-                </div>
-                <div className="explorecard">
-                    <img
-                        src="https://aphrodite.gmanetwork.com/entertainment/articles/480_360_40__20210430124335.jpg"
-                        alt="all"
-                        className="card__img"
-                        style={{ marginTop: "-25px" }}
-                    />
-                    <span className="card__footer">
-                        <span>MEMES</span>
-                        <span>100 NFT's</span>
-                    </span>
-                    <span className="card__action">
-                        <svg viewBox="0 0 448 512" title="play">
-                            <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
-                        </svg>
-                    </span>
-                </div>
-                <div className="explorecard">
-                    <img
-                        src="https://i.seadn.io/s/raw/files/e7f6a2735321dfbad153ebef6d2333ca.jpg?auto=format&dpr=1&w=384"
-                        alt="art"
-                        className="card__img"
-                    />
-                    <span className="card__footer">
-                        <span>PFP</span>
-                        <span>15 NFT's</span>
                     </span>
                     <span className="card__action">
                         <svg viewBox="0 0 448 512" title="play">
